0cd82305b4f89d428a2839bd36336422d405f287
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2025-04-24 11:26:50 +0700

    edit axios

--- instagram/src/lib/axios.ts
+++ instagram/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.NEXT_BASE_URL_BE! || "http://localhost:8000/api";
+const BASE_URL = process.env.NEXT_BASE_URL_BE! || "https://be-ig.vercel.app/api";
 
 export default axios.create({
   baseURL: BASE_URL,
